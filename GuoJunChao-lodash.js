061765527f748aa0b32f453ff56ed665ec36962f
kong <1104300535@qq.com>
2016-11-16 23:03:46 +0800

    lodash

---
+++ GuoJunChao-lodash.js
@@ -0,0 +1,68 @@
+/*
+ *
+ */
+
+
+
+var GuoJunChao = {
+	chunk: function(arr, n) {
+
+		var l = Math.ceil(arr.length / n)
+
+		var result = []
+
+		for (i = 0; i < l; i++) {
+			result[i] = []
+		}
+
+		for (var i = 0; i < arr.length; i++) {
+			result[Math.floor(i / n)][i % n] = arr[i]
+		}
+
+		return result
+	},
+
+	compact: function(array) {
+
+		var result = []
+
+		for (i = 0; i < array.length; i++) {
+
+			if (array[i]) {
+
+				result.push(array[i])
+
+			}
+
+		}
+
+		return result
+	},
+
+	concat: function(array, other) {
+		var newArr = []
+		for (i = 0; i < arguments.length; i++) {
+			if (typeof arguments[i] == 'object') {
+				for (j = 0; j < arguments[i].length; j++) {
+					newArr.push(arguments[i][j])
+				}
+			} else {
+				newArr.push(arguments[i])
+			}
+		}
+		return newArr
+	},
+
+	difference: function(arr_1, arr_2) {
+		var str_1 = arr_1.join('')
+		for (i = 0; i < arr_2.length; i++) {
+			str_1 = str_1.replace(arr_2[i], "")
+		}
+		var result = str_1.split('')
+		for (j = 0; j < result.length; j++) {
+			result[j] = +result[j]
+		}
+		return result
+	},
+
+}
